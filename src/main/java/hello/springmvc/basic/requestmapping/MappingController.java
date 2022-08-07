824cf8de62c27c5f160c0407f0bf02d7d3e53a5d
hyuntae <bht9011@gmail.com>
2022-08-07 12:49:54 +0900

    as

--- src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -2,10 +2,8 @@ package hello.springmvc.basic.requestmapping;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MappingController {
@@ -34,6 +32,51 @@ public class MappingController {
     @GetMapping("/mapping-get-v2")
     public String mappingGetV2() {
         log.info("mappingGetV2");
+        return "ok";
+    }
+
+    /**
+     * PathVariable 사용
+     * 변수명이 같으면 생략 가능
+     * @param data
+     * @return
+     */
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable("userId") String data) {
+        log.info("mapping Path userId={}", data);
+        return "ok";
+    }
+
+    /**
+     * 다중 매핑
+     */
+    @GetMapping("/mapping/users/{userId}/orders/{orderId}")
+    public String mappingPath(@PathVariable String userId, @PathVariable String orderId) {
+        log.info("mapping userId={}, orderId={}", userId, orderId);
+        return "ok";
+    }
+
+    @GetMapping(value = "/mapping-param", params = "mode=debug")
+    public String mappingParam() {
+        log.info("mappingParam");
+        return "ok";
+    }
+
+    @GetMapping(value = "/mapping-param", headers = "mode=debug")
+    public String mappingHeaders() {
+        log.info("mappingHeaders");
+        return "ok";
+    }
+
+    @PostMapping(value = "/mapping-consume", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String mappingConsume() {
+        log.info("mappingConsume");
+        return "ok";
+    }
+
+    @PostMapping(value = "mapping-produce", produces = MediaType.TEXT_HTML_VALUE)
+    public String mappingProduce() {
+        log.info("mappingProduce");
         return "ok";
     }
 }
